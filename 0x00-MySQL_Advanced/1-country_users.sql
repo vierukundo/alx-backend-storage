f63dc10a1218f284fc3fafc0ac6da4c187a7cfa4
vierukundo <vierukundo@gmail.com>
2024-01-19 23:52:01 +0000

    script that creates table with enum attribute

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,12 @@
+-- This script creates a table with these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null (default is US)
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
