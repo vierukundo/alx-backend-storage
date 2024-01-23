bbad31b6a0f68a720e47e6ec5a8e6bdda6cd3292
vierukundo <vierukundo@gmail.com>
2024-01-23 15:23:24 -0800

    updates the collections by name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    query = {"name": name}
+    update_data = {"$set": {"topics": topics}}
+    mongo_collection.update_one(query, update_data)
