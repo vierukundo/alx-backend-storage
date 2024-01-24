4ab4c9defb8d80cb1d07811cc1d6ae26d7cd5cdd
vierukundo <vierukundo@gmail.com>
2024-01-23 16:13:41 -0800

    prints collection nginx stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats
+about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+
+def print_stats():
+    client = MongoClient('localhost', 27017)
+    db = client.logs
+    collection = db.nginx
+
+    # Total number of logs
+    total_logs = collection.count_documents({})
+    print('{} logs'.format(total_logs))
+
+    # Count of each method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print('\tmethod {}: {}'.format(method, method_count))
+
+    # Count of logs with method=GET and path=/status
+    status_check_count = collection.count_documents(
+            {"method": "GET", "path": "/status"})
+    print('{} status check'.format(status_check_count))
+
+
+if __name__ == "__main__":
+    print_stats()
