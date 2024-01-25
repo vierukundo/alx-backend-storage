4916f108531c804ac3b1dd03ffc2f6d5d87542f6
vierukundo <vierukundo@gmail.com>
2024-01-25 19:38:36 +0000

    Implemented gets methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
-from typing import Union
+from typing import Union, Callable
 import redis
 import uuid
 
@@ -17,3 +17,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """function that retrieves data from redis"""
+        data = self._redis.get(key)
+        if fn and data:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Retrieve a string from the cache using the specified key."""
+        return str(self._redis.get(key))
+
+    def get_int(self, key: str) -> int:
+        """Retrieve an integer from the cache using the specified key."""
+        return int(self._redis.get(key))
